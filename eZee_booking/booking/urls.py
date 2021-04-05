e20f2899164cc3c4e29c5c15ff021be58b417f1d
Rinku994 <rinkudahal0@gmail.com>
2021-04-05 21:44:19 +0545

    code added

--- eZee_booking/booking/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, review,booking
-
-urlpatterns = [
-    path('', home, name = 'home'),
-    path('bookings', booking, name = 'booking'),
-    path('review', review, name = 'review'),
-]
